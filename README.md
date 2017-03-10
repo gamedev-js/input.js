653fa0641b8f9c81397d0dfab4994fa767c59973
Johnny Wu <KarasAya@gmail.com>
2017-03-11 01:08:02 +0800

    update

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-input.click(key, downupSpan, dragDistance)
-input.dbclick(key, downupSpan, clickSpan, dragDistance)
-options: lockPointer
-options: useDragMask
+## Input
+
+Update input states every frame.
+
+## TODO
+
+ - input.click(key, downupSpan, dragDistance)
+ - input.dbclick(key, downupSpan, clickSpan, dragDistance)
+ - options: lockPointer
+ - options: useDragMask
--- index.js
+++ index.js
@@ -10,6 +10,8 @@ export default class Input {
     this._lastTime = 0;
     this._pointerLocked = false;
 
+    this._mouseEnabled = false;
+
     // the mouse state
     this._mouse = {
       x: 0,
@@ -29,6 +31,11 @@ export default class Input {
 
     // mousemove
     this._mousemoveHandle = event => {
+      if (!this._mouseEnabled) {
+        return;
+      }
+
+      event.preventDefault();
       event.stopPropagation();
 
       if (this._pointerLocked) {
@@ -43,6 +50,7 @@ export default class Input {
 
     // mousewheel
     this._mousewheelHandle = event => {
+      event.preventDefault();
       event.stopPropagation();
 
       this._mouse.scrollX = event.deltaX;
@@ -51,25 +59,86 @@ export default class Input {
 
     // mousedown
     this._mousedownHandle = event => {
+      // NOTE: this will prevent mouse enter the text selection state.
+      event.preventDefault();
       event.stopPropagation();
 
-      console.log('mousedown', event.button);
-      console.log('mousedown', event.buttons);
+      // left mouse down
+      if (event.button === 0) {
+        this._element.focus();
+      }
+
+      //
+      if (document.body.requestPointerLock && !this._pointerLocked) {
+        document.body.requestPointerLock();
+        this._pointerLocked = true;
+      }
     };
 
     // mouseup
     this._mouseupHandle = event => {
+      event.preventDefault();
       event.stopPropagation();
 
-      console.log('mouseup', event.button);
-      console.log('mouseup', event.buttons);
+      //
+      if ( document.exitPointerLock && this._pointerLocked ) {
+        this._pointerLocked = false;
+        document.exitPointerLock();
+      }
+
+      // DISABLE:
+      // let bcr = this._element.getBoundingClientRect();
+      // this._mouse.dx = event.movementX;
+      // this._mouse.dy = event.movementY;
+      // this._mouse.prevX = this._mouse.x = event.clientX - bcr.left;
+      // this._mouse.prevX = this._mouse.y = event.clientY - bcr.top;
+    };
+
+    // mouseenter
+    this._mouseenterHandle = event => {
+      event.preventDefault();
+      event.stopPropagation();
+
+      let bcr = this._element.getBoundingClientRect();
+
+      this._mouseEnabled = true;
+      this._mouse.dx = 0.0;
+      this._mouse.dy = 0.0;
+      this._mouse.prevX = this._mouse.x = event.clientX - bcr.left;
+      this._mouse.prevX = this._mouse.y = event.clientY - bcr.top;
+    };
+
+    // mouseleave
+    this._mouseleaveHandle = event => {
+      event.preventDefault();
+      event.stopPropagation();
+
+      let bcr = this._element.getBoundingClientRect();
+
+      this._mouseEnabled = false;
+      this._mouse.dx = event.movementX;
+      this._mouse.dy = event.movementY;
+      this._mouse.prevX = this._mouse.x = event.clientX - bcr.left;
+      this._mouse.prevX = this._mouse.y = event.clientY - bcr.top;
     };
 
     this._registerEvents();
   }
 
   _registerEvents () {
-    document.addEventListener('mousedown', this._mousedownHandle);
+    this._element.addEventListener('mousedown', this._mousedownHandle);
+    document.addEventListener('mouseup', this._mouseupHandle);
+    this._element.addEventListener('mouseenter', this._mouseenterHandle);
+    this._element.addEventListener('mouseleave', this._mouseleaveHandle);
+    document.addEventListener('mousemove', this._mousemoveHandle);
+  }
+
+  get mouseX () {
+    return this._mouse.x;
+  }
+
+  get mouseY () {
+    return this._mouse.y;
   }
 
   reset () {
@@ -84,5 +153,13 @@ export default class Input {
     this._mouse.scrollY = 0;
 
     // update keyboard states
+  }
+
+  destroy () {
+    this._element.removeEventListener('mousedown', this._mousedownHandle);
+    document.removeEventListener('mouseup', this._mouseupHandle);
+    this._element.removeEventListener('mouseenter', this._mouseenterHandle);
+    this._element.removeEventListener('mouseleave', this._mouseleaveHandle);
+    document.removeEventListener('mousemove', this._mousemoveHandle);
   }
 }
