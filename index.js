47eee5424c4adac564431e1d3ebf2e6ca2ce25a7
Johnny Wu <karasaya@gmail.com>
2018-01-17 14:41:59 +0800

    consider intertY option

--- index.js
+++ index.js
@@ -124,7 +124,11 @@ export default class Input {
 
       if (this._pointerLocked) {
         this._mouse.x += event.movementX;
-        this._mouse.y -= event.movementY;
+        if (this._opts.invertY) {
+          this._mouse.y -= event.movementY;
+        } else {
+          this._mouse.y += event.movementY;
+        }
       } else {
         this._mouse.x = this._calcOffsetX(event.clientX);
         this._mouse.y = this._calcOffsetY(event.clientY);
